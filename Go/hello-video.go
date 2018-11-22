aaaa8982b1df22ed86e19e715d3783ad343b1319
ekapilik <erickapilik@gmail.com>
2018-11-22 20:42:35 +0000

    gocv video test

---
+++ Go/hello-video.go
@@ -0,0 +1,18 @@
+package main
+
+
+import (
+	"gocv.io/x/gocv"
+)
+
+func main() {
+	webcam, _ := gocv.VideoCaptureDevice(3470)
+	window := gocv.NewWindow("Hello")
+	img := gocv.NewMat()
+
+	for {
+		webcam.Read(&img)
+		window.IMShow(img)
+		window.WaitKey(1)
+	}
+}
